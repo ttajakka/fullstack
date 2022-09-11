6ebddc28671de9f66ec4bdd0a69d17e7134f4d0c
ttajakka <tuomas.tajakka@gmail.com>
2022-09-11 17:22:25 +0200

    add blogs controller and Mongoose model

--- osa4/blogilista/backend/index.js
+++ osa4/blogilista/backend/index.js
@@ -1,20 +1,13 @@
-require('dotenv').config()
-const http = require('http')
+//require('dotenv').config()
+//const http = require('http')
 const express = require('express')
 const app = express()
 const cors = require('cors')
 const mongoose = require('mongoose')
 const logger = require('./utils/logger')
 const config = require('./utils/config')
+const blogsRouter = require('./controllers/blogs')
 
-const blogSchema = mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number
-})
-
-const Blog = mongoose.model('Blog', blogSchema)
 
 mongoose.connect(config.MONGODB_URI)
   .then(() => {
@@ -24,26 +17,11 @@ mongoose.connect(config.MONGODB_URI)
     logger.error('error connectingto MongoDB:', error.message)
   })
 
+
 app.use(cors())
 app.use(express.json())
 
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
+app.use('/api/blogs', blogsRouter)
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
