e717c24e411a66fb6db05266cce06644e38a846a
ttajakka <tuomas.tajakka@gmail.com>
2022-09-11 17:34:44 +0200

    separate app into index.js and app.js

--- osa4/blogilista/backend/index.js
+++ osa4/blogilista/backend/index.js
@@ -1,14 +1,15 @@
+const app = require('./app')
 //require('dotenv').config()
-//const http = require('http')
-const express = require('express')
-const app = express()
-const cors = require('cors')
-const mongoose = require('mongoose')
-const logger = require('./utils/logger')
+const http = require('http')
+//const express = require('express')
+//const app = express()
+//const cors = require('cors')
+//const mongoose = require('mongoose')
 const config = require('./utils/config')
-const blogsRouter = require('./controllers/blogs')
+const logger = require('./utils/logger')
+//const blogsRouter = require('./controllers/blogs')
 
-
+/*
 mongoose.connect(config.MONGODB_URI)
   .then(() => {
     logger.info('connected to MongoDB')
@@ -16,14 +17,16 @@ mongoose.connect(config.MONGODB_URI)
   .catch((error) => {
     logger.error('error connectingto MongoDB:', error.message)
   })
+*/
 
+//app.use(cors())
+//app.use(express.json())
 
-app.use(cors())
-app.use(express.json())
+//app.use('/api/blogs', blogsRouter)
 
-app.use('/api/blogs', blogsRouter)
+const server = http.createServer(app)
 
-const PORT = process.env.PORT
-app.listen(PORT, () => {
-  logger.info(`Server running on port ${PORT}`)
+//const PORT = process.env.PORT
+server.listen(config.PORT, () => {
+  logger.info(`Server running on port ${config.PORT}`)
 })
