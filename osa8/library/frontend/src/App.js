96b6147c430e712d82e68c31ec390629b558ba3f
ttajakka <tuomas.tajakka@gmail.com>
2023-05-23 21:31:23 +0300

    add 8.8

---
+++ osa8/library/frontend/src/App.js
@@ -0,0 +1,34 @@
+import { useState } from 'react'
+import { useQuery } from '@apollo/client'
+import Authors from './components/Authors'
+import Books from './components/Books'
+import NewBook from './components/NewBook'
+import { ALL_AUTHORS } from './queries'
+
+const App = () => {
+  const [page, setPage] = useState('authors')
+
+  const authors = useQuery(ALL_AUTHORS)
+
+  if (page === 'authors' && authors.loading)  {
+    return <div>loading...</div>
+  }
+
+  return (
+    <div>
+      <div>
+        <button onClick={() => setPage('authors')}>authors</button>
+        <button onClick={() => setPage('books')}>books</button>
+        <button onClick={() => setPage('add')}>add book</button>
+      </div>
+
+      <Authors authors={authors.data.allAuthors} show={page === 'authors'} />
+
+      <Books show={page === 'books'} />
+
+      <NewBook show={page === 'add'} />
+    </div>
+  )
+}
+
+export default App
