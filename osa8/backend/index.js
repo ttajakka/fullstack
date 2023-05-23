b0e4f473ef3db300c38806688f24b681afad61bb
ttajakka <tuomas.tajakka@gmail.com>
2023-05-23 15:28:21 +0300

    add 8.7

--- osa8/backend/index.js
+++ osa8/backend/index.js
@@ -114,6 +114,11 @@ const typeDefs = `
       author: String!
       genres: [String!]!
     ): Book
+
+    editAuthor(
+      name: String
+      setBornTo: Int
+    ): Author
   }
 `
 
@@ -146,8 +151,18 @@ const resolvers = {
         authors = authors.concat(newAuthor)
       }
       return book
-    }
-  }
+    },
+
+    editAuthor: (root, args) => {
+      const author = authors.find(a => a.name === args.name)
+      if (!author) {
+        return null
+      }
+      const updatedAuthor = { ...author, born: args.setBornTo }
+      authors = authors.map(a => a.name === args.name ? updatedAuthor : a)
+      return updatedAuthor
+    },
+  },
 }
 
 const server = new ApolloServer({
