01d8b546a3b14687327e4af58873ad3fbfe33b8c
ttajakka <tuomas.tajakka@gmail.com>
2023-07-04 22:08:34 +0300

    add 9.10 and 9.11

--- osa9/patientor/backend/src/index.ts
+++ osa9/patientor/backend/src/index.ts
@@ -1,6 +1,9 @@
 import express from 'express';
 import cors from 'cors';
 
+import diagnosesRouter from './routes/diagnoses';
+import patientRouter from './routes/patients';
+
 const app = express();
 
 app.use(express.json());
@@ -12,6 +15,10 @@ app.get('/api/ping', (_req, res) => {
   console.log('someone pinged here');
   res.send('pong');
 });
+
+app.use('/api/diagnoses', diagnosesRouter);
+
+app.use('/api/patients', patientRouter);
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
