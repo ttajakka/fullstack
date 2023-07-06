541e12325a58e590befd4aa3e21b9afe3f0d3a78
ttajakka <tuomas.tajakka@gmail.com>
2023-07-06 20:20:23 +0300

    add 9.18

--- osa9/flight_diary/frontend/src/App.tsx
+++ osa9/flight_diary/frontend/src/App.tsx
@@ -1,10 +1,10 @@
- import { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import { getAllDiaries, createDiaryEntry } from './diaryService';
 import { NonSensitiveDiaryEntry, Visibility, Weather } from '../../backend/src/types';
 
 const Diary = (props: NonSensitiveDiaryEntry): JSX.Element => {
   return (<div>
-    <h3>{props.date}</h3>
+    <h4>{props.date}</h4>
     <div>visibility: {props.visibility}</div>
     <div>weather: {props.weather}</div>
   </div>);
@@ -16,6 +16,7 @@ const App = () => {
   const [newVisibility, setNewVisibility] = useState('');
   const [newWeather, setNewWeather] = useState('');
   const [newComment, setNewComment] = useState('');
+  const [errorMessage, setErrorMessage] = useState('');
 
   useEffect(() => {
     getAllDiaries().then(data =>
@@ -23,37 +24,45 @@ const App = () => {
   });
 
   const entryCreation = (event: React.SyntheticEvent) => {
-    event.preventDefault()
-    const newEntry = { 
+    event.preventDefault();
+    const newEntry = {
       date: newDate,
       visibility: newVisibility as Visibility,
       weather: newWeather as Weather,
       comment: newComment
-    }
-    createDiaryEntry(newEntry).then(data => {
-      setDiaries(diaries.concat(data))
-    })
-  }
+    };
+    createDiaryEntry(newEntry)
+      .then(data => {
+        if (data) {
+          setDiaries(diaries.concat(data));
+        }
+      })
+      .catch(error => { 
+        setErrorMessage(error.message)
+      })
+  };
 
   return (
     <div>
-      <h2>Add new entry</h2>
+      <h3>Add new entry</h3>
+      <div style={{ color: "red" }}>{errorMessage}</div>
+      <br />
       <form onSubmit={entryCreation}>
         <div>
-        date<input value={newDate} onChange={(event) => setNewDate(event.target.value)} />
+          date<input value={newDate} onChange={(event) => setNewDate(event.target.value)} />
         </div>
         <div>
-        visibility<input value={newVisibility} onChange={(event) => setNewVisibility(event.target.value)} />
+          visibility<input value={newVisibility} onChange={(event) => setNewVisibility(event.target.value)} />
         </div>
         <div>
-        weather<input value={newWeather} onChange={(event) => setNewWeather(event.target.value)} />
+          weather<input value={newWeather} onChange={(event) => setNewWeather(event.target.value)} />
         </div>
         <div>
-        comment<input value={newComment} onChange={(event) => setNewComment(event.target.value)} />
+          comment<input value={newComment} onChange={(event) => setNewComment(event.target.value)} />
         </div>
         <button type='submit'>add</button>
       </form>
-      <h2>Diary entries</h2>
+      <h3>Diary entries</h3>
       {diaries.map(d => <Diary key={d.id} {...d} />)}
     </div>
   );
--- osa9/flight_diary/frontend/src/diaryService.tsx
+++ osa9/flight_diary/frontend/src/diaryService.tsx
@@ -1,16 +1,22 @@
-import axios from 'axios'
-import { NewDiaryEntry, NonSensitiveDiaryEntry } from '../../backend/src/types'
+import axios from 'axios';
+import { NewDiaryEntry, NonSensitiveDiaryEntry } from '../../backend/src/types';
 
-const baseUrl = 'http://localhost:3001/api/diaries'
+const baseUrl = 'http://localhost:3001/api/diaries';
 
 export const getAllDiaries = () => {
     return axios
         .get<NonSensitiveDiaryEntry[]>(baseUrl)
-        .then(response => response.data)
-}
+        .then(response => response.data);
+};
 
 export const createDiaryEntry = (object: NewDiaryEntry) => {
-    return axios
-        .post<NonSensitiveDiaryEntry>(baseUrl, object)
+    return axios.post<NonSensitiveDiaryEntry>(baseUrl, object)
         .then(response => response.data)
-}
+        .catch(error => {
+            if (axios.isAxiosError(error) && error.response) {
+                throw new Error(error.response.data);
+            } else {
+                console.log("from services", error);
+            }
+        });
+};
