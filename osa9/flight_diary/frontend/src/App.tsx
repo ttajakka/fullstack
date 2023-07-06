b23d24777af4f2058dd46ad5f7f27370f84e9a61
ttajakka <tuomas.tajakka@gmail.com>
2023-07-06 19:28:48 +0300

    add 9.17

--- osa9/flight_diary/frontend/src/App.tsx
+++ osa9/flight_diary/frontend/src/App.tsx
@@ -1,29 +1,62 @@
-import { useState, useEffect } from 'react';
-import { getAllDiaries } from './diaryService';
-import { NonSensitiveDiaryEntry } from '../../backend/src/types';
+ import { useState, useEffect } from 'react';
+import { getAllDiaries, createDiaryEntry } from './diaryService';
+import { NonSensitiveDiaryEntry, Visibility, Weather } from '../../backend/src/types';
 
 const Diary = (props: NonSensitiveDiaryEntry): JSX.Element => {
-  return(<div>
-    <h2>{props.date}</h2>
+  return (<div>
+    <h3>{props.date}</h3>
     <div>visibility: {props.visibility}</div>
     <div>weather: {props.weather}</div>
-  </div>)
-}
+  </div>);
+};
 
 const App = () => {
   const [diaries, setDiaries] = useState<NonSensitiveDiaryEntry[]>([]);
+  const [newDate, setNewDate] = useState('');
+  const [newVisibility, setNewVisibility] = useState('');
+  const [newWeather, setNewWeather] = useState('');
+  const [newComment, setNewComment] = useState('');
 
   useEffect(() => {
-    getAllDiaries().then(data => 
-      setDiaries(data))
-  })
+    getAllDiaries().then(data =>
+      setDiaries(data));
+  });
+
+  const entryCreation = (event: React.SyntheticEvent) => {
+    event.preventDefault()
+    const newEntry = { 
+      date: newDate,
+      visibility: newVisibility as Visibility,
+      weather: newWeather as Weather,
+      comment: newComment
+    }
+    createDiaryEntry(newEntry).then(data => {
+      setDiaries(diaries.concat(data))
+    })
+  }
 
   return (
     <div>
-      <h1>Diary entries</h1>
+      <h2>Add new entry</h2>
+      <form onSubmit={entryCreation}>
+        <div>
+        date<input value={newDate} onChange={(event) => setNewDate(event.target.value)} />
+        </div>
+        <div>
+        visibility<input value={newVisibility} onChange={(event) => setNewVisibility(event.target.value)} />
+        </div>
+        <div>
+        weather<input value={newWeather} onChange={(event) => setNewWeather(event.target.value)} />
+        </div>
+        <div>
+        comment<input value={newComment} onChange={(event) => setNewComment(event.target.value)} />
+        </div>
+        <button type='submit'>add</button>
+      </form>
+      <h2>Diary entries</h2>
       {diaries.map(d => <Diary key={d.id} {...d} />)}
     </div>
   );
-}
+};
 
 export default App;
--- osa9/flight_diary/frontend/src/diaryService.tsx
+++ osa9/flight_diary/frontend/src/diaryService.tsx
@@ -1,10 +1,16 @@
 import axios from 'axios'
-import { NonSensitiveDiaryEntry } from '../../backend/src/types'
+import { NewDiaryEntry, NonSensitiveDiaryEntry } from '../../backend/src/types'
 
 const baseUrl = 'http://localhost:3001/api/diaries'
 
 export const getAllDiaries = () => {
     return axios
         .get<NonSensitiveDiaryEntry[]>(baseUrl)
+        .then(response => response.data)
+}
+
+export const createDiaryEntry = (object: NewDiaryEntry) => {
+    return axios
+        .post<NonSensitiveDiaryEntry>(baseUrl, object)
         .then(response => response.data)
 }
