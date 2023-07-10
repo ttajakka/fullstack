84ca082d428fb9242c086101c5136612051f9161
ttajakka <tuomas.tajakka@gmail.com>
2023-07-10 16:30:51 +0300

    add 9.23

--- osa9/patientor/frontend/src/components/PatientPage/index.tsx
+++ osa9/patientor/frontend/src/components/PatientPage/index.tsx
@@ -7,7 +7,18 @@ import TransgenderIcon from '@mui/icons-material/Transgender';
 
 
 import patientService from "../../services/patients";
-import { Patient } from "../../types";
+import { Entry, Patient } from "../../types";
+
+const EntryDiv = (data: Entry) => {
+    return (<div>
+        <div>{data.date} <em>{data.description}</em></div>
+        {data.diagnosisCodes ?
+            <ul>
+                {data.diagnosisCodes.map(d => <li key={d}>{d}</li>)}
+            </ul> : null}
+    </div>);
+};
+
 
 const PatientPage = () => {
     const [patient, setPatient] = useState<Patient | null>(null);
@@ -30,13 +41,15 @@ const PatientPage = () => {
             <h2>
                 {patient.name}{patient.gender === "male"
                     ? <MaleIcon /> :
-                    patient.gender === "female" ? 
-                    <FemaleIcon /> : <TransgenderIcon/>}
+                    patient.gender === "female" ?
+                        <FemaleIcon /> : <TransgenderIcon />}
             </h2>
             <div>date of birth: {patient.dateOfBirth}</div>
             <div>gender: {patient.gender}</div>
             <div>ssn: {patient.ssn}</div>
             <div>occupation: {patient.occupation}</div>
+            <h3>Entries</h3>
+            {patient.entries.map((e) => <EntryDiv key={e.id} {...e} />)}
         </div>
     );
 };
