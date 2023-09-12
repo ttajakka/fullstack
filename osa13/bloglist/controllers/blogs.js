7f0d5d40547453fba6b0adc9656e599f804e7121
ttajakka <tuomas.tajakka@gmail.com>
2023-09-12 22:32:00 +0300

    add exercise 13.6

--- osa13/bloglist/controllers/blogs.js
+++ osa13/bloglist/controllers/blogs.js
@@ -3,32 +3,45 @@ const router = require('express').Router()
 const { Blog } = require('../models')
 
 router.get('/', async (req, res) => {
-    const blogs = await Blog.findAll()
-    res.json(blogs)
-  })
-  
-  router.post('/', async (req, res) => {
-    try {
-      const blog = await Blog.create(req.body)
-      return res.json(blog)
-    } catch (error) {
-      return res.status(400).json({ error })
-    }
-  })
+  const blogs = await Blog.findAll()
+  res.json(blogs)
+})
 
-  const blogFinder = async (req, res, next) => {
-    req.blog = await Blog.findByPk(req.params.id)
-    next()
+router.post('/', async (req, res) => {
+  try {
+    const blog = await Blog.create(req.body)
+    return res.json(blog)
+  } catch (error) {
+    return res.status(400).json({ error })
   }
-  
-  router.delete('/:id', blogFinder, async (req, res) => {
-    // const blog = await Blog.findByPk(req.params.id)
-    if (req.blog) {
-      await req.blog.destroy()
-      res.json(req.blog)
-    } else {
-      res.status(404).end()
-    }
-  })
+})
 
-  module.exports = router
+const blogFinder = async (req, res, next) => {
+  req.blog = await Blog.findByPk(req.params.id)
+  next()
+}
+
+router.delete('/:id', blogFinder, async (req, res) => {
+  if (req.blog) {
+    await req.blog.destroy()
+    res.json(req.blog)
+  } else {
+    res.status(404).end()
+  }
+})
+
+router.put('/:id', blogFinder, async (req, res) => {
+  if (!req.body.likes) {
+    res.status(400).end()
+  }
+
+  if (req.blog) {
+    req.blog.likes = req.body.likes
+    await req.blog.save()
+    res.json(req.blog)
+  } else {
+    res.status(404).end()
+  }
+})
+
+module.exports = router
