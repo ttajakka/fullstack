a43472bfaa6371edd7edf47c0b69d7d54f618a0c
ttajakka <tuomas.tajakka@gmail.com>
2023-09-13 11:22:34 +0300

    add 13.7

--- osa13/bloglist/controllers/blogs.js
+++ osa13/bloglist/controllers/blogs.js
@@ -2,17 +2,22 @@ const router = require('express').Router()
 
 const { Blog } = require('../models')
 
-router.get('/', async (req, res) => {
-  const blogs = await Blog.findAll()
-  res.json(blogs)
+router.get('/', async (req, res, next) => {
+  try {
+    const blogs = await Blog.findAll()
+    res.json(blogs)
+  } catch (error) {
+    next(error)
+  }
 })
 
-router.post('/', async (req, res) => {
+router.post('/', async (req, res, next) => {
   try {
     const blog = await Blog.create(req.body)
     return res.json(blog)
   } catch (error) {
-    return res.status(400).json({ error })
+    // return res.status(400).json({ error })
+    next(error)
   }
 })
 
@@ -21,27 +26,47 @@ const blogFinder = async (req, res, next) => {
   next()
 }
 
-router.delete('/:id', blogFinder, async (req, res) => {
-  if (req.blog) {
-    await req.blog.destroy()
-    res.json(req.blog)
-  } else {
-    res.status(404).end()
+router.delete('/:id', blogFinder, async (req, res, next) => {
+  try {
+    if (req.blog) {
+      await req.blog.destroy()
+      res.json(req.blog)
+    } else {
+      res.status(404).end()
+    }
+  } catch (error) {
+    next(error)
   }
 })
 
-router.put('/:id', blogFinder, async (req, res) => {
+router.put('/:id', blogFinder, async (req, res, next) => {
   if (!req.body.likes) {
     res.status(400).end()
   }
 
-  if (req.blog) {
-    req.blog.likes = req.body.likes
-    await req.blog.save()
-    res.json(req.blog)
-  } else {
-    res.status(404).end()
+  try {
+    if (req.blog) {
+      req.blog.likes = req.body.likes
+      await req.blog.save()
+      res.json(req.blog)
+    } else {
+      res.status(404).end()
+    }
+  } catch (error) {
+    next(error)
   }
 })
+
+const errorHandler = (error, req, res, next) => {
+  console.log(error)
+
+  if (error.name === 'SequelizeValidationError') {
+    res.status(400).send(error.message)
+  }
+
+  next(error)
+}
+
+router.use(errorHandler)
 
 module.exports = router
